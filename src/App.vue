2339807278977da58cdcea62af163663f2620a21
ilic93 <markoilic.va93@gmail.com>
2020-11-20 14:44:39 +0100

    static page created and styled

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <header>
+      <h1>Weather App</h1>
+      <p>Search your city or other ones to see what's the weather like</p>
+    </header>
+
+    <form>
+      <input type="text" placeholder="Enter city name.." v-model="city"/>
+    </form>
+
+    <div class="info">
+      <div class="place">Valjevo, Serbia</div>
+      <div class="date">20.11.2020</div>
+      <br>
+      <div class="temp">10°C</div>
+      <div class="weather">Rain</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      city: ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Goldman&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Goldman', cursive;
 }
+
+#app {
+  text-align: center;
+  background-image: url('./assets/sunny-weather.jpg');
+  min-height: 100vh;
+  background-size: cover;
+  padding: 20px;
+}
+
+header {
+  font-size: 20px;
+  margin: 20px 0;
+}
+
+input {
+  width: 100%;
+  font-size: 25px;
+  padding: 10px;
+  color: black;
+
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  border-radius: 15px 0;
+  border: none;
+  transition: 0.5s;
+}
+
+input:focus {
+  background-color: rgba(255, 255, 255, 0.8); 
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.7);
+  border-radius: 0 15px;
+}
+
+.info {
+  color: white;
+  margin-top: 20px;
+  font-size: 40px;
+}
+
+.place, .date, .temp, .weather {
+  text-shadow: 2px 2px rgba(0, 0, 0, 1);
+}
+
+.date {
+  font-style: italic;
+  font-weight: 200;
+}
+
+.temp {
+  display: inline-block;
+  font-size: 90px;
+  font-weight: 700;
+  background-color: rgba(255, 255, 255, 0.3);
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 15px;
+  box-shadow: 5px 5px rgba(0, 0, 0, 0.3);
+}
+
+
 </style>
